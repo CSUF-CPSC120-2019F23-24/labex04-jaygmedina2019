8cb8040a917be317937856b189f56f89211fbf86
jayg.medina00 <jayg.medina00@cs104u-4e.acad.fullerton.edu>
2019-09-14 14:44:43 -0700

    created code to solve the sales for next year

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // Sales prediction
+#include<iostream>
+
+int main()
+  {
+    int year_2018 = 2103419277;
+    int year_2019 = (2103419277 * .18);
+
+    //gives you the sales from last year
+    std::cout << "Last years sales were $ " <<year_2018 << std::endl;
+
+    //gives the amount of the predicted sales
+    std::cout << "The year 2020 is estimated to be: $ "<<year_2019;
+    return 0;
+
+
+
+
+
+
+
+
+  }
